ee173276d3d53437adcee594218d75848b041305
James Prior <jim.prior@accenture.com>
2022-06-23 17:59:03 -0400

    Add more tests for "Backstage passes ...".
    Co-authored-by: James Prior <jim.prior@accenture.com>

--- gilded_rose/test/gildedRoseTest.cc
+++ gilded_rose/test/gildedRoseTest.cc
@@ -124,8 +124,23 @@ INSTANTIATE_TEST_SUITE_P(
             Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)
         },
         TestDataStruct {
+            "BackstagePass_99_50_DecreaseSellIn",
+            Item("Backstage passes to a TAFKAL80ETC concert", 99, 50),
+            Item("Backstage passes to a TAFKAL80ETC concert", 98, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_99_49_DecreaseSellInAndIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 99, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", 98, 50)
+        },
+        TestDataStruct {
             "BackstagePass_11_50_DecreaseSellIn",
             Item("Backstage passes to a TAFKAL80ETC concert", 11, 50),
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_11_49_DecreaseSellInAndIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 11, 49),
             Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)
         },
         TestDataStruct {
@@ -139,9 +154,34 @@ INSTANTIATE_TEST_SUITE_P(
             Item("Backstage passes to a TAFKAL80ETC concert", 9, 50)
         },
         TestDataStruct {
-            "BackstagePass_11_49_DecreaseSellIn",
-            Item("Backstage passes to a TAFKAL80ETC concert", 11, 49),
-            Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)
+            "BackstagePass_6_51_DecrementsSellIn",
+            Item("Backstage passes to a TAFKAL80ETC concert", 6, 51),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 51)
+        },
+        TestDataStruct {
+            "BackstagePass_6_50_DecrementsSellIn",
+            Item("Backstage passes to a TAFKAL80ETC concert", 6, 50),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_6_49_DecrementsSellInIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 6, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_6_48_DecrementsSellInDoubleIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 6, 48),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_6_47_DecrementsSellInDoubleIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 6, 47),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)
+        },
+        TestDataStruct {
+            "BackstagePass_5_51_DecrementsSellIn",
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 51),
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 51)
         },
         TestDataStruct {
             "BackstagePass_5_50_DecrementsSellIn",
@@ -152,6 +192,121 @@ INSTANTIATE_TEST_SUITE_P(
             "BackstagePass_5_49_DecrementsSellInAndIncrementsQuality",
             Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
             Item("Backstage passes to a TAFKAL80ETC concert", 4, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_5_48_DecrementsSellInAndDoubleIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 48),
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_5_47_DecrementsSellInAndTripleIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 47),
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_5_46_DecrementsSellInAndTripleIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 46),
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 49)
+        },
+        TestDataStruct {
+            "BackstagePass_1_51_DecrementsSellIn",
+            Item("Backstage passes to a TAFKAL80ETC concert", 1, 51),
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 51)
+        },
+        TestDataStruct {
+            "BackstagePass_1_50_DecrementsSellIn",
+            Item("Backstage passes to a TAFKAL80ETC concert", 1, 50),
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_1_49_DecrementsSellInAndIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 1, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_1_48_DecrementsSellInAndDoubleIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 1, 48),
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_1_47_DecrementsSellInAndTripleIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 1, 47),
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)
+        },
+        TestDataStruct {
+            "BackstagePass_1_46_DecrementsSellInAndTripleIncrementsQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 1, 46),
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 49)
+        },
+        TestDataStruct {
+            "BackstagePass_0_51_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 51),
+            Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_0_50_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 50),
+            Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_0_49_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_0_48_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 48),
+            Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_0_47_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 47),
+            Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_0_46_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 46),
+            Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_0_45_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 45),
+            Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_Minus99_51_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", -99, 51),
+            Item("Backstage passes to a TAFKAL80ETC concert", -100, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_Minus99_50_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", -99, 50),
+            Item("Backstage passes to a TAFKAL80ETC concert", -100, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_Minus99_49_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", -99, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", -100, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_Minus99_48_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", -99, 48),
+            Item("Backstage passes to a TAFKAL80ETC concert", -100, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_Minus99_47_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", -99, 47),
+            Item("Backstage passes to a TAFKAL80ETC concert", -100, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_Minus99_46_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", -99, 46),
+            Item("Backstage passes to a TAFKAL80ETC concert", -100, 0)
+        },
+        TestDataStruct {
+            "BackstagePass_Minus99_45_DecrementsSellInAndZerosQuality",
+            Item("Backstage passes to a TAFKAL80ETC concert", -99, 45),
+            Item("Backstage passes to a TAFKAL80ETC concert", -100, 0)
         },
         TestDataStruct {
             "Sulfuras_0_1_Unchanged",
